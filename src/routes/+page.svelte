fcff6b84caee22284f8462fc211810afd119c289
shashi089 <shashidharnaik8@gmail.com>
2025-11-24 22:10:41 +0530

    css update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
 <div class="bg-blue-500 text-white p-4 rounded">
     Sveltekit Login Page
 </div>
+
+<div class="mt-8 flex justify-center">
+<a href="/login" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Login
+</a>
+
+</div>
